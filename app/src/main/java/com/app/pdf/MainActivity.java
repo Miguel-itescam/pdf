2808ebe44a62876fa8077c67f53c3b518f020af6
miguel-camara <miguelordonezcamara@hotmail.com>
2023-08-18 23:46:49 -0600

    test

--- app/src/main/java/com/app/pdf/MainActivity.java
+++ app/src/main/java/com/app/pdf/MainActivity.java
@@ -6,8 +6,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.ParcelFileDescriptor;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.itextpdf.text.BadElementException;
@@ -24,6 +26,8 @@ import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.Barcode128;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.shockwave.pdfium.PdfDocument;
+import com.shockwave.pdfium.PdfiumCore;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
@@ -53,11 +57,44 @@ public class MainActivity extends AppCompatActivity {
         Font FONT_FOOTER = new Font(Font.FontFamily.HELVETICA, 18, Font.NORMAL);
 
         try {
+            ImageView iv = (ImageView) findViewById(R.id.imageView);
+
             File pdfFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "prueba.pdf");
-            PdfWriter.getInstance(documento, new FileOutputStream(pdfFile));
+            //PdfWriter.getInstance(documento, new FileOutputStream(pdfFile));
+
+            //
+            ParcelFileDescriptor fb = ParcelFileDescriptor.open(pdfFile, ParcelFileDescriptor.MODE_READ_ONLY);
+
+            PdfiumCore pdfiumCore = new PdfiumCore(this);
+
+            int pageNum = 0;
+
+            PdfDocument pdfDocument = pdfiumCore.newDocument(fb);
+
+            pdfiumCore.openPage(pdfDocument, pageNum);
+
+            int width = pdfiumCore.getPageWidthPoint(pdfDocument, pageNum);
+            int height = pdfiumCore.getPageHeightPoint(pdfDocument, pageNum);
+
+            // ARGB_8888 - best quality, high memory usage, higher possibility of OutOfMemoryError
+            // RGB_565 - little worse quality, twice less memory usage
+            Bitmap bitmap = Bitmap.createBitmap(width, height,
+                    Bitmap.Config.RGB_565);
+            pdfiumCore.renderPageBitmap(pdfDocument, bitmap, pageNum, 0, 0,
+                    width, height);
+            //if you need to render annotations and form fields, you can use
+            //the same method above adding 'true' as last param
+
+            iv.setImageBitmap(bitmap);
+
+            printInfo(pdfiumCore, pdfDocument);
+
+            pdfiumCore.closeDocument(pdfDocument); // important!
+
+
+
+/*
             documento.open();
-
-
             documento.add(createTableImage(30, R.mipmap.ic_google_foreground, 0, 0));
             documento.add(createTable(FONT_TITLE, "LS24R350FZLXZX", 0, 50));
             documento.add(createTable(FONT_FOOTER, "LS24R350FZLXZX", 10, 0));
@@ -67,16 +104,18 @@ public class MainActivity extends AppCompatActivity {
             documento.add(createTable(FONT_FOOTER, "https://www.amazon.com.mx/SAMSUNG-LS24R350FZLXZX-Monitor-Experiencia-inmersiva/dp/B09PRSV4MR?ref_=Oct_DLandingS_D_7c6e4843_0", 10, 0));
 
             documento.close();
-
-            Toast.makeText(this, "GG", Toast.LENGTH_SHORT).show();
+            */
+            Toast.makeText(this, "Creado", Toast.LENGTH_SHORT).show();
 
         }catch(Exception e) {
-            Log.e("createPDF", "error: " + e.getMessage(), e);
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
 
-    }
 
+
+
+    }
+/*
     private PdfPTable createTableImage( int width, int source, int top, int bottom)  {
         PdfPTable table = new PdfPTable(1);
 
@@ -122,4 +161,72 @@ public class MainActivity extends AppCompatActivity {
         }
         return img;
     }
+
+
+    void openPdf() {
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+
+        try {
+            ParcelFileDescriptor fb = ParcelFileDescriptor.open(new File("/sdcard/test.pdf"), ParcelFileDescriptor.MODE_READ_ONLY);
+            PdfiumCore pdfiumCore = new PdfiumCore(this);
+        }catch (Exception e) {
+            Log.e("openPdf", "error: " + e.getMessage(), e);
+        }
+
+//        ParcelFileDescriptor fd = ...;
+        int pageNum = 0;
+        PdfiumCore pdfiumCore = new PdfiumCore(this);
+        try {
+            com.shockwave.pdfium.PdfDocument pdfDocument = pdfiumCore.newDocument(fd);
+
+            pdfiumCore.openPage(pdfDocument, pageNum);
+
+            int width = pdfiumCore.getPageWidthPoint(pdfDocument, pageNum);
+            int height = pdfiumCore.getPageHeightPoint(pdfDocument, pageNum);
+
+            // ARGB_8888 - best quality, high memory usage, higher possibility of OutOfMemoryError
+            // RGB_565 - little worse quality, twice less memory usage
+            Bitmap bitmap = Bitmap.createBitmap(width, height,
+                    Bitmap.Config.RGB_565);
+            pdfiumCore.renderPageBitmap(pdfDocument, bitmap, pageNum, 0, 0,
+                    width, height);
+            //if you need to render annotations and form fields, you can use
+            //the same method above adding 'true' as last param
+
+            iv.setImageBitmap(bitmap);
+
+            printInfo(pdfiumCore, pdfDocument);
+
+            pdfiumCore.closeDocument(pdfDocument); // important!
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+*/
+    public void printInfo(PdfiumCore core, PdfDocument doc) {
+        PdfDocument.Meta meta = core.getDocumentMeta(doc);
+        Log.e(null, "title = " + meta.getTitle());
+        Log.e(null, "author = " + meta.getAuthor());
+        Log.e(null, "subject = " + meta.getSubject());
+        Log.e(null, "keywords = " + meta.getKeywords());
+        Log.e(null, "creator = " + meta.getCreator());
+        Log.e(null, "producer = " + meta.getProducer());
+        Log.e(null, "creationDate = " + meta.getCreationDate());
+        Log.e(null, "modDate = " + meta.getModDate());
+
+        printBookmarksTree(core.getTableOfContents(doc), "-");
+
+    }
+
+    public void printBookmarksTree(List<PdfDocument.Bookmark> tree, String sep) {
+        for (PdfDocument.Bookmark b : tree) {
+
+            Log.e(null, String.format("%s %s, p %d", sep, b.getTitle(), b.getPageIdx()));
+
+            if (b.hasChildren()) {
+                printBookmarksTree(b.getChildren(), sep + "-");
+            }
+        }
+    }
+
 }
